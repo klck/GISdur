b01c047f8f4dd62ba131cbae0d46a94d3ab66cd1
unknown <Reitzo@pcrzp864.AD.Uni-Marburg.DE>
2014-05-14 13:26:49 +0200

    return as list

--- R/dreieck.r
+++ R/dreieck.r
@@ -2,7 +2,7 @@ dreieck <- function(alpha, c) {
   a <- sin(alpha * pi/180) * c
   b <- cos(alpha * pi/180) * c
   
-  werte <- c(a, b)
-  werte
+  werte <- list(x<-round(a,2)y<-round(b,2)
+  return(werte)
   
 }
