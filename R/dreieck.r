626dca0db703d6c7e3f742c8a729e5fc5a73a9ad
reitzo <reitz.oliver@gmx.de>
2014-05-15 22:27:15 +0200

    test commit

--- R/dreieck.r
+++ R/dreieck.r
@@ -1,5 +1,5 @@
 dreieck <- function(alpha, c) {
-  a <- sin(alpha * pi/180) * c 481054
+  a <- sin(alpha * pi/180) * c
   b <- cos(alpha * pi/180) * c
   
   werte <- list(x<-round(a,2),y<-round(b,2))
