339a19bc2658ba858eed54f23439395a473490d2
klck <te.kloeckner@yahoo.de>
2014-05-15 10:23:25 +0200

    Fehler behoben

--- R/dreieck.r
+++ R/dreieck.r
@@ -2,7 +2,7 @@ dreieck <- function(alpha, c) {
   a <- sin(alpha * pi/180) * c
   b <- cos(alpha * pi/180) * c
   
-  werte <- list(x<-round(a,2)y<-round(b,2)
+  werte <- list(x<-round(a,2),y<-round(b,2))
   return(werte)
   
 }
