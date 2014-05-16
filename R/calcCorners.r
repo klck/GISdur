d0a5a533030de8ac2bc79aed80d4d01e4b06121e
reitzo <reitz.oliver@gmx.de>
2014-05-16 13:33:09 +0200

    erster Versuch für calcCorners

---
+++ R/calcCorners.r
@@ -0,0 +1,32 @@
+calcCorners <- function(x.known, y.known, 
+                        orientation, declination,
+                        size.x, size.y) {
+  alpha <- orientation + declination
+  alpha270 <- alpha + 270
+  alpha90 <- alpha + 90
+  
+  aA <- sin(alpha270 * pi/180) * size.x
+  bA <- cos(alpha270 * pi/180) * size.x
+  
+  aB <- sin(alpha * pi/180) * size.y
+  bB <- cos(alpha * pi/180) * size.y
+  
+  aC <- sin(alpha90 * pi/180) * size.x
+  bC <- cos(alpha90 * pi/180) * size.x
+  
+  
+  A <- list(x.known + bA, y.known + aA)
+  B <- list(x.known + bB, y.known + aB)
+  C <- list(x.known + bC, y.known + aC)
+  D <- list(x.known, y.known)
+  
+  Corners <- list(A, B, C, D)
+  return(Corners)
+  
+}
+
+calcCorners(481054, 5645540, 9, 1, 48, 30)
+
+# Bisherige Überlegung: 1. Umrechungsfaktor (declintation) wird gebraucht; 
+# habe hier zum Probieren "1" genommen
+# das mit alpha ist noch sehr unschön, da nur auf unseren Fall anwendbar.
