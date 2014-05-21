341873f0240fd3596bde1dd6d4e36bd2cf96d0e7
klck <te.kloeckner@yahoo.de>
2014-05-21 13:55:55 +0200

    13:55

--- R/tree_loc.R
+++ R/tree_loc.R
@@ -1,3 +1,16 @@
 fls<-list.files("data",pattern="locations.csv",full.names=TRUE)
 fls
 
+loc.list <- lapply(fls, function(...) {
+  read.csv(..., stringsAsFactors = FALSE, fill = TRUE)
+})
+str(loc.list)
+loc.list
+loc <- do.call("rbind", loc.list)
+str(loc)
+spec <- read.csv("data/species_inventory.csv",stringsAsFactors = FALSE, fill = TRUE)
+spec
+str(spec)
+loc.spec<-merge(loc,spec)
+str(loc.spec)
+save(loc.spec, file = "data/tree_locations_species_all.RData")
